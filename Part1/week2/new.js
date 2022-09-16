48f3d8c6377ea6d0c4eef8366363dd88b168c7a1
Mateo Feria-Estrada <mferiaestrada@my.bcit.ca>
2022-09-16 12:15:28 -0700

    adding more code

---
+++ Part1/week2/new.js
@@ -0,0 +1,31 @@
+// Boolean 
+
+var proceed = true;
+var i = 0;
+
+while (proceed) {
+    i++; // i = 1
+    proceed = false;
+};
+
+console.log(i);
+
+// Data type - object 
+/** 
+ * 6-4-1 Create a variable called grannySmith add the 
+ * following values (use camelCase)
+ * colour of the apple is green 
+ * orgin is location is australia 
+ * origin date is 1868
+ * Then console log out the colour from 
+ * the obkect.
+*/
+
+var grannySmith = {
+    color: "green",
+    originLocation: "australia",
+    orginDate: 1868
+
+};
+
+console.log(grannySmith);
